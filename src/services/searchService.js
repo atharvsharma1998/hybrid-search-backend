eebf3b0ada9498b155c2485aff5c01fdf6b97f7d
atharvsharma1998 <atharv228.as@gmail.com>
2024-09-02 13:06:32 +0100

    error changes

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -88,7 +88,7 @@ const client = new Client({
             return { results: [] , timeTaken: 0}; // Return empty results with 0 time if no hits
         }
     } catch (error) {
-        console.error('Elasticsearch search error:', error);
+        console.error('Elasticsearch search error:', error.id);
         throw error;    // throwing the error to the controller
     }
     
